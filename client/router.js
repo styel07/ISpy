bb338282c64f8df3e895bb91a065aca50bc476e5
kasi <kasionaga@gmail.com>
2015-10-10 16:19:56 -1000

    hi francis

--- client/router.js
+++ client/router.js
@@ -1,16 +1,28 @@
-//Link router.js to Application Layout
+"use strict";
+
 Router.configure({
-  layoutTemplate : 'BaseLayout'
+  layoutTemplate : 'BaseLayout',   // default layout always rendered;
+  before : function () {
+    if (!Meteor.user()) {
+      if (Meteor.loggingIn()) {
+        // just wait if logging in;
+        // console.log('Router: logging in');
+      } else {
+        this.redirect('login');
+        // console.log('Router: no user');
+      }
+    } else if (Router.current().route.getName() === 'login') {  // else user is loggedIn;
+      this.redirect('main');
+      // console.log('Router: redirecting');
+    }
+    // else
+    //   console.log('Router: user exists');
+    this.next();    // must call next() to get the Router to continue executing;
+  }
 });
 
-// ============= Home Page ================
-Router.route('/', function () {
-  this.render('login', {
-  });
-});
-
-
-// =========== tasks/new =================
-Router.route('/main', function() {
-  this.render('main');
-});
+Router.route('/', { template: '' });
+Router.route('/login', { template: '' });
+Router.route('/main');
+// Router.route('/dashboard', { template: ''});
+// By default the router will render the capitalized name of the template, with punctuations removed and next letter capped.
